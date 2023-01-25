fd028e7b51e3e6cf7786d2c5a0bf9a53b82f3a45
EC2 Default User <ec2-user@ip-172-31-5-72.ap-northeast-1.compute.internal>
2023-01-25 10:45:47 +0000

    [コントローラーの記述を追記しました]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,20 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    
+    following = current_user.relationships.build(followed_id:params[:user_id])
+    following.save
+    redirect_to request.referrer
+  end
+  
+  def destroy
+    following = current_user.relationships.fing_by(followed_id:params[:user_id])
+  　following.destroy
+  　redirect_to request.referrer
+  end
+  
+  private
+  # フォロー一覧ページとフォロワー一覧ページ用のコントローラを記述
+  
 end
