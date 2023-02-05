586e9bfda78e0c69077b321c16d8d569ffa775f8
EC2 Default User <ec2-user@ip-172-31-5-72.ap-northeast-1.compute.internal>
2023-02-05 13:20:53 +0000

    [詳細画面に検索機能実装しました]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
+  
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books
+    @book = Book.new
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
+    end
+  end
 
   def show
     @user = User.find(params[:id])
